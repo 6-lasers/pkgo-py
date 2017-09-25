f409af94b0a7f0869a9471113184ac67e92a70cd
Ted Chen (XBOX HARDWARE LASER ARMADA) <techen@microsoft.com>
2017-09-25 15:24:43 -0700

    Update readme, remove 'ball type' argument for pinap

--- README.md
+++ README.md
@@ -19,3 +19,13 @@ Frontend for the CP calculation library. Given a Pokemon species, target level, 
 Usage: calcCost.py \<start level\> \<end level\>
 
 Similar to Gamepress' CP calculator, calculates the stardust and candy cost associated with powering up a Pokemon from a given start level to a target level.
+
+## catchCalc.py
+Usage: catchCalc.py \<species name or catch rate\> \<Pokemon level\> \<throw bonus\> \<ball type\> \<berry type\> \<curve ball\> \<medal type\> \<ball count\> \<allow flee\>
+
+Similar to Gamepress' catch calculator and raid catch calculator, determines the catch chances of a given Pokemon. If *allow flee* is True, behaves like the catch calculator--otherwise, behaves like the raid catch calculator.
+
+## pinap.py
+Usage: pinap.py \<species name or catch rate\> \<Pokemon level\> \<throw bonus\> \<curve ball\> \<medal type\> \<ball count\> \<will transfer\>
+
+Given a raid scenario, determine on which ball you should switch to throwing Golden Razz in order to maximize your expected candy returns.
--- pinap.py
+++ pinap.py
@@ -6,61 +6,64 @@
 #  Calculate the breakpoint at which to throw Pinaps
 #  to maximize candy from a raid boss.
 #
-#  Usage: pinap.py <species name or catch rate> <Pokemon level> <throw bonus> <ball type> <curve ball> <medal type> <ball count>
+#  Usage: pinap.py <species name or catch rate> <Pokemon level> <throw bonus> <curve ball> <medal type> <ball count> <will transfer>
 #
 ######################################################
 
 import sys
 import optparse
+import distutils.util
 
 import catchlib
 
 def main(argv=None):
-    usage="calcCatch.py <species name or catch rate> <Pokemon level> <throw bonus> <ball type> <curve ball> <medal type> <ball count>"
+    usage="calcCatch.py <species name or catch rate> <Pokemon level> <throw bonus> <curve ball> <medal type> <ball count> <will transfer>"
     parser = optparse.OptionParser(usage=usage)
     
     (options, args) = parser.parse_args()
     
     # Get arguments
     try:
-        base, level, throw_type, ball, curve, medal, ball_count = args
+        base, level, throw_type, curve, medal, ball_count, will_transfer = args
     except ValueError:
         print "ERROR: Invalid number of arguments"
         print usage
         return 1
     
+    # Interpret "will_transfer" argument as a boolean
+    will_transfer = distutils.util.strtobool(will_transfer)
+    
+    candy_award = 3
+    transfer_award = 0
+    if will_transfer:
+        transfer_award = 1
+    
     # Store breakpoint with and without transfer
     opt_candy = 0
-    opt_candy_transfer = 0
     opt_candy_index = 0
-    opt_candy_index_transfer = 0
     
     # Loop from 0 to <number of balls> to find the breakpoint
     for i in range(int(ball_count) + 1):
-        print "====Start GRB on ball {0}====".format(i)
+        print "====Start GRB with {0} balls remaining====".format(i)
         
         # Calculate catch rates
-        p_catch, p_cumu_catch = catchlib.calcCatch(base, level, throw_type, ball, "pinap", curve, medal, int(ball_count) - i, False)
-        grb_catch, grb_cumu_catch = catchlib.calcCatch(base, level, throw_type, ball, "grb", curve, medal, i, False)
+        p_catch, p_cumu_catch = catchlib.calcCatch(base, level, throw_type, "premier", "pinap", curve, medal, int(ball_count) - i, False)
+        grb_catch, grb_cumu_catch = catchlib.calcCatch(base, level, throw_type, "premier", "grb", curve, medal, i, False)
         grb_adjusted_catch = (1 - p_cumu_catch) * grb_cumu_catch
         print "Pinap catch rate: {0:2}%, GRB catch rate: {1:2}%".format(p_cumu_catch * 100, grb_adjusted_catch * 100)
         total_catch = p_cumu_catch + grb_adjusted_catch
         print "Total catch rate: {0:2}%".format(total_catch * 100)
         
         # Calculate expected candy
-        exp_candy = (6 * p_cumu_catch) + (3 * grb_adjusted_catch)
-        exp_candy_transfer = (7 * p_cumu_catch) + (4 * grb_adjusted_catch)
-        print "Expected candies: {0:2}, (if transferring) {1:2}".format(exp_candy, exp_candy_transfer)
+        exp_candy = (((2 * candy_award) + transfer_award) * p_cumu_catch) + ((candy_award + transfer_award) * grb_adjusted_catch)
+        print "Expected candies: {0:2}".format(exp_candy)
         
         if exp_candy > opt_candy:
             opt_candy = exp_candy
             opt_candy_index = i
-        if exp_candy_transfer > opt_candy_transfer:
-            opt_candy_transfer = exp_candy_transfer
-            opt_candy_index_transfer = i
     
     # Final recommendation
-    print "Start GRB on ball {0} if transferring, else {1}".format(opt_candy_index_transfer, opt_candy_index)
+    print "Start GRB when you have {0} balls left".format(opt_candy_index)
     
     return 0
 
